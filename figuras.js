4289071e686c65ee58e47bdf955cff512da2e3d3
Mateo <arismendyuribemateo@gmail.com>
2021-11-20 23:21:59 -0500

    Implementé funciones para obtener el perimetro y area de las funciones con cualquier valor

--- figuras.js
+++ figuras.js
@@ -2,49 +2,39 @@
 
 console.group("Cuadrados");
 
+function perimetroCuadrado(ladoCuadrado){
+    return ladoCuadrado*4; 
+}
 
-
-const ladoCuadrado = 5; 
-console.log("La medida de los lados del cuadrado son: " + ladoCuadrado + "cm");
-
-const perimetroCuadrado = ladoCuadrado * 4; 
-console.log("El perímetro del cuadrado es: " + perimetroCuadrado + "cm");
-
-const areaCuadrado = ladoCuadrado ** 2;
-console.log("El área del cuadrado es: " + areaCuadrado + "cm^2");
-
+function areaCuadrado(ladoCuadrado) {
+    return ladoCuadrado **2; 
+}
 console.groupEnd();
 
 //CÓDIGO DE TRIANGULO
 console.group("Triangulos"); 
 
-const ladoTriangulo1 = 6; 
-const ladoTriangulo2 = 6; 
-const baseTriangulo = 4; 
-const alturaTriangulo = 5.5;
-console.log("La medida de los lados del triangulo son: " + ladoTriangulo1 + "cm, " + ladoTriangulo2 + "cm, " + baseTriangulo + "cm");
+function perimetroTriangulo(lado1, lado2, baseT){
+    return lado1 + lado2 + baseT;
+}
 
-const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo; 
-console.log("El perímetro del triangulo es: " + perimetroTriangulo + "cm");
-
-const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
-console.log("El área del triangulo es: " + areaTriangulo + "cm^2");
+function areaTriangulo(baseT, alturaT){
+    return (baseT*alturaT)/2;
+}
 
 console.groupEnd(); 
-
 
 //CÓDIGO DE CIRCULO
 console.group("Circulos")
 
-const radioCirculo = 4;
-console.log("El rádio del circulo es de: " + radioCirculo + "cm");
-
-const diametroCiruclo = radioCirculo * 2; 
-console.log("El diametro del circulo es de: " + diametroCiruclo + "cm");
 const pi = Math.PI; 
+function circunferenciaCirculo (radio){
+    diametro = radio * 2;
+    return diametro * pi;
+}
 
-const circunferenciaCirculo = pi * diametroCiruclo; 
-console.log("La circunferencia del circulo es de: " + circunferenciaCirculo + "cm"); 
+function areaCirculo(radio) {
+    return pi * (radio ** 2);
+}
 
-const areaCirculo = pi * (radioCirculo**2); 
-console.log("El área del circulo es de: " + areaCirculo + "cm^2");
+console.groupEnd();
